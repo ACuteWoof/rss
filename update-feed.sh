ed6107398425eede37d905181c8ed792a20b44ca
acutewoof <sutharshanvithushan@gmail.com>
2026-04-19 11:47:32 +0530

    shorter details message

--- update-feed.sh
+++ update-feed.sh
@@ -20,7 +20,7 @@ while true; do
 				mkdir "$thedir"
 				cd "$thedir"
 				displaydate=$(date -d "$pubDate")
-				echo "This file contains the details about this specific RSS item.\nClosing vim will put you in the directory containing the downloaded content.\nSee the last lines for more.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$description\n\n---\n\nYou have to decide what to do with the downloaded content. This script does not automate the opening since malicious programs could be executed through the feeds. Always check the content with a text editor or some other safe method before opening it with its handler." > details.md
+				echo "This is a summary.\nClosing vim will put you in the directory containing the downloaded content.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$description\n" > details.md
 				echo "[$thedate] [$name] $title [$displaydate] $description | $(pwd)" >> $INDEX
 				notify-send "New post on $name" "$title\n$displaydate\n\n$description"
 				wget "$url"
