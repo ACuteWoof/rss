a980abcd034e883857ae2f76f104ae849628ded4
ACuteWoof <woof@lewoof.xyz>
2026-04-19 13:02:44 +0530

    sort order

--- browse-rss.sh
+++ browse-rss.sh
@@ -1,3 +1,3 @@
 INDEX="/home/acutewoof/basement/rss/index.txt"
 
-sort $INDEX | dmenu -i -p 'RSS:' -l 10 | uxterm -e "cd \"$(sed -e "s/.*|//" -e "s/^ \//\//")\" && vim details.md; zsh"
+sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | uxterm -e "cd \"$(sed -e "s/.*|//" -e "s/^ \//\//")\" && vim details.md; zsh"
