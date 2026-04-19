9ca7f15fd403b0ce8fbb066b49685fc7614b3788
acutewoof <sutharshanvithushan@gmail.com>
2026-04-19 11:09:20 +0530

    notifications, fix udpate

--- update-feed.sh
+++ update-feed.sh
@@ -8,14 +8,21 @@ while true; do
 		provider_dir="$STORE/$dir"
 		mkdir -p "$provider_dir"
 		cd "$provider_dir"
-		wget "$url" -O "feed.xml"
-		xml sel -t -m "//item" -v "link" -o "|" -v "guid" -o "|" -v "pubDate" -o "|" -v "title" -n feed.xml > items.txt
-		while read -r url guid pubDate; do
-			mkdir "$guid"
-			cd "$guid"
-			echo "$link\n$guid\n$pubDate\n$title" > details.txt
-			wget "$url"
-			cd ..
+		wget "$url" -O "unprocessed-feed.xml"
+		echo $(tr '\n' ' ' < unprocessed-feed.xml) > feed.xml
+		xml sel -T -t -m "//item" -v "link" -o "|" -v "guid" -o "|" -v "pubDate" -o "|" -v "title" -o "|" -v "description" -n feed.xml > items.txt
+		while read -r url guid pubDate title description; do
+			thedate=$(date -d "$pubDate" +%s)
+			thedir="./$thedate-$guid"
+			if [ ! -d "./$thedir" ]; then
+				mkdir "$thedir"
+				cd "$thedir"
+				displaydate=$(date -d "$pubDate")
+				echo "---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$description" > details.md
+				notify-send "New post on $name" "$title\n$displaydate\n\n$description"
+				wget "$url"
+				cd ..
+			fi
 		done < items.txt
 	done < $PROV
 
