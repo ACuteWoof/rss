da5fbe53a9090bea956b3894b5dc6a9dcfe72e7c
acutewoof <sutharshanvithushan@gmail.com>
2026-04-20 06:14:41 +0530

    deal with bloated sites, unique lines on index

--- update-feed.sh
+++ update-feed.sh
@@ -20,22 +20,25 @@ while true; do
 		sed -i 's/\t/ /g' items.txt
 		while read -r url guid pubDate title description; do
 			thedate=$(date -d "$pubDate" +%s)
-			guid = $(sed 's|/|-|g' < "$guid")
-			thedir="./$thedate-$guid"
+			processedguid=$(echo "$guid" | sed 's|/|-|g')
+			thedir="./$thedate-$processedguid"
 			if [ ! -d "./$thedir" ]; then
 				mkdir "$thedir" || break
 				cd "$thedir"
 				displaydate=$(date -d "$pubDate")
-				echo "This is a summary and contains the markdown of any HTML files.\nClosing vim will put you in the directory containing the downloaded content.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$description\n" > details.md
-				echo "[$thedate] [$name] $title [$displaydate] $description | $(pwd)" >> $INDEX
+				echo "$description" > rss-description.html
+				mddesc=$(eval $HTMLTOMD "rss-description.html")
+				echo "This is a summary and contains the markdown of any HTML files.\nClosing vim will put you in the directory containing the downloaded content.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$mddesc\n" > details.md
+				indexline="[$thedate] [$name] $title [$displaydate] $description | $(pwd)" 
+				grep -qxF $indexline $INDEX || echo $indexline >> $INDEX
 				notify-send "New post on $name" "$title\n$displaydate\n\n$description"
 				if [ "${url#*https://youtube.com}" != "$url" ]; then
 					yt-dlp "$url" --cookies-from-browser $YTDL_BROWSER:$YTDL_COOKIES
 				fi
 				wget -p -k -H -E -nd --reject js -e robots=off "$url"
 				for htmlfile in *.html; do
-					echo "---\n$htmlfile\n---\n\n$(eval $HTMLTOMD $htmlfile)" >> details.md
 					[ -e "$htmlfile" ] || continue
+					echo "---\n$htmlfile\n---\n\n$(eval $HTMLTOMD $htmlfile)" >> details.md
 				done
 				cd ..
 			fi
