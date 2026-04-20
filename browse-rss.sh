ad2bb4ee94bf722eef8f2fcd426f3772c8c2eef6
acutewoof <sutharshanvithushan@gmail.com>
2026-04-20 07:11:26 +0530

    more variables so that changes can be made to adapt IFS and DELIM

--- browse-rss.sh
+++ browse-rss.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 INDEX="/home/acutewoof/basement/rss/index.txt"
+DELIM="|"
 
-sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | uxterm -e "cd \"$(sed -e "s/.*|//" -e "s/^ \//\//")\" && vim details.md; zsh"
+sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | uxterm -e "cd \"$(sed -e "s/.*$DELIM//" -e "s/^ \//\//")\" && vim details.md; zsh"
--- update-feed.sh
+++ update-feed.sh
@@ -7,6 +7,7 @@ HTMLTOMD="html2tet"
 YTDL_BROWSER="chromium"
 YTDL_COOKIES="~/.local/share/qutebrowser/webengine"
 IFS="|"
+DMENU_DELIM="|"
 DELAY=300
 
 while true; do
@@ -16,8 +17,7 @@ while true; do
 		cd "$provider_dir"
 		wget "$url" -O "unprocessed-feed.xml"
 		echo $(tr '\n' ' ' < unprocessed-feed.xml) > feed.xml
-		xml sel -T -t -m "//item" -v "link" -o "|" -v "guid" -o "|" -v "pubDate" -o "|" -v "title" -o "|" -v "description" -n feed.xml > items.txt || xml sel -T -t -m "//item" -v "link" -o "|" -v "guid" -o "|" -v "pubDate" -o "|" -v "title" -o "|No description" -n feed.xml > items.txt
-		sed -i 's/\t/ /g' items.txt
+		xml sel -T -t -m "//item" -v "link" -o "$IFS" -v "guid" -o "$IFS" -v "pubDate" -o "$IFS" -v "title" -o "$IFS" -v "description" -n feed.xml > items.txt $IFS$IFS xml sel -T -t -m "//item" -v "link" -o "$IFS" -v "guid" -o "$IFS" -v "pubDate" -o "$IFS" -v "title" -o "$IFS No description" -n feed.xml > items.txt
 		while read -r url guid pubDate title description; do
 			thedate=$(date -d "$pubDate" +%s)
 			processedguid=$(echo "$guid" | sed 's|/|-|g')
@@ -29,7 +29,7 @@ while true; do
 				echo "$description" > rss-description.html
 				mddesc=$(eval $HTMLTOMD "rss-description.html")
 				echo "This is a summary and contains the markdown of any HTML files.\nClosing vim will put you in the directory containing the downloaded content.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$mddesc\n" > details.md
-				indexline="[$thedate] [$name] $title [$displaydate] $description | $(pwd)" 
+				indexline="[$thedate] [$name] $title [$displaydate] $description $DMENU_DELIM $(pwd)" 
 				grep -qxF $indexline $INDEX || echo "$indexline" >> $INDEX
 				notify-send "New post on $name" "$title\n$displaydate\n\n$description"
 				if [ "${url#*https://youtube.com}" != "$url" ]; then
