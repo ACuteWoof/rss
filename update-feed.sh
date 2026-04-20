8120340cd36a63e49015ec74ef5a73e8a3b7c9fc
acutewoof <sutharshanvithushan@gmail.com>
2026-04-20 10:20:51 +0530

    use markdown for notification and index

--- update-feed.sh
+++ update-feed.sh
@@ -3,7 +3,7 @@
 PROV="/home/acutewoof/basement/rss/providers.txt"
 INDEX="/home/acutewoof/basement/rss/index.txt"
 STORE="/home/acutewoof/basement/rss"
-HTMLTOMD="html2tet"
+HTMLTOMD="html2text"
 YTDL_BROWSER="chromium"
 YTDL_COOKIES="~/.local/share/qutebrowser/webengine"
 IFS="|"
@@ -28,10 +28,11 @@ while true; do
 				displaydate=$(date -d "$pubDate")
 				echo "$description" > rss-description.html
 				mddesc=$(eval $HTMLTOMD "rss-description.html")
+				shortmddesc=$(echo "$mddesc" | tr '\n' ' ')
 				echo "This is a summary and contains the markdown of any HTML files.\nClosing vim will put you in the directory containing the downloaded content.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$mddesc\n" > details.md
-				indexline="[$thedate] [$name] $title [$displaydate] $description $DMENU_DELIM $(pwd)" 
+				indexline="[$thedate] [$name] $title [$displaydate] $shortmddesc $DMENU_DELIM $(pwd)" 
 				grep -qxF $indexline $INDEX || echo "$indexline" >> $INDEX
-				notify-send "New post on $name" "$title\n$displaydate\n\n$description"
+				notify-send "New post on $name" "$title\n$displaydate\n\n$mddesc"
 
 				# to download youtube links as videos instead of as a website
 				# use more if statements like this to handle downloading from other feeds
