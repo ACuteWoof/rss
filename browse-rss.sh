5cd89e1947fdc75b7efd2eba6a05a487757fd259
acutewoof <sutharshanvithushan@gmail.com>
2026-04-20 06:33:43 +0530

    sh

--- browse-rss.sh
+++ browse-rss.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 INDEX="/home/acutewoof/basement/rss/index.txt"
 
 sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | uxterm -e "cd \"$(sed -e "s/.*|//" -e "s/^ \//\//")\" && vim details.md; zsh"
