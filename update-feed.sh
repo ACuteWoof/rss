07670c2b4f127cd7d5a10ae1bbc183c0b92c8cd5
acutewoof <sutharshanvithushan@gmail.com>
2026-04-20 11:25:43 +0530

    pdf generation

--- update-feed.sh
+++ update-feed.sh
@@ -17,7 +17,7 @@ while true; do
 		cd "$provider_dir"
 		wget "$url" -O "unprocessed-feed.xml"
 		echo $(tr '\n' ' ' < unprocessed-feed.xml) > feed.xml
-		xml sel -T -t -m "//item" -v "link" -o "$IFS" -v "guid" -o "$IFS" -v "pubDate" -o "$IFS" -v "title" -o "$IFS" -v "description" -n feed.xml > items.txt $IFS$IFS xml sel -T -t -m "//item" -v "link" -o "$IFS" -v "guid" -o "$IFS" -v "pubDate" -o "$IFS" -v "title" -o "$IFS No description" -n feed.xml > items.txt
+		xml sel -T -t -m "//item" -v "link" -o "$IFS" -v "guid" -o "$IFS" -v "pubDate" -o "$IFS" -v "title" -o "$IFS" -v "description" -n feed.xml > items.txt || xml sel -T -t -m "//item" -v "link" -o "$IFS" -v "guid" -o "$IFS" -v "pubDate" -o "$IFS" -v "title" -o "$IFS No description" -n feed.xml > items.txt
 		while read -r url guid pubDate title description; do
 			thedate=$(date -d "$pubDate" +%s)
 			processedguid=$(echo "$guid" | sed 's|/|-|g')
@@ -26,10 +26,10 @@ while true; do
 				mkdir "$thedir" || break
 				cd "$thedir"
 				displaydate=$(date -d "$pubDate")
-				echo "$description" > rss-description.html
-				mddesc=$(eval $HTMLTOMD "rss-description.html")
+				echo "$description" > rss-description
+				mddesc=$(eval $HTMLTOMD "rss-description")
 				shortmddesc=$(echo "$mddesc" | tr '\n' ' ')
-				echo "This is a summary and contains the markdown of any HTML files.\nClosing vim will put you in the directory containing the downloaded content.\n\n---\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n---\n\n$mddesc\n" > details.md
+				echo "This is a summary and contains the markdown of any HTML files.\n\n---\n\nurl: $url\nguid: $guid\ndate: $pubDate\ndate (local): $displaydate\ntitle: $title\n\n---\n\n$mddesc\n" > details.md
 				indexline="[$thedate] [$name] $title [$displaydate] $shortmddesc $DMENU_DELIM $(pwd)" 
 				grep -qxF $indexline $INDEX || echo "$indexline" >> $INDEX
 				notify-send "New post on $name" "$title\n$displaydate\n\n$mddesc"
@@ -46,6 +46,7 @@ while true; do
 					[ -e "$htmlfile" ] || continue
 					echo "---\n$htmlfile\n---\n\n$(eval $HTMLTOMD $htmlfile)" >> details.md
 				done
+				pandoc --embed-resources details.md -s -o details.pdf
 				cd ..
 			fi
 		done < items.txt
