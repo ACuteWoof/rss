72940f98bf54a6710f22f9cbcb0178cbb1077ec4
acutewoof <sutharshanvithushan@gmail.com>
2026-04-20 13:11:43 +0530

    fallback to md

--- browse-rss.sh
+++ browse-rss.sh
@@ -4,7 +4,7 @@ INDEX="/home/acutewoof/basement/rss/index.txt"
 DELIM="|"
 
 # for PDF:
-sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | sed -e "s/.*$DELIM//" -e "s/^ \//\//" | xargs -r -I @ sh -c 'cd @; zathura details.pdf; uxterm'
+sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | sed -e "s/.*$DELIM//" -e "s/^ \//\//" | xargs -r -I @ sh -c 'cd @; xdg-open details.pdf || uxterm -e vim details.md ; uxterm'
 
 # for MD:
 # sort -r $INDEX | dmenu -i -p 'RSS:' -l 10 | uxterm -e "cd \"$(sed -e "s/.*$DELIM//" -e "s/^ \//\//")\" && vim details.md; zsh"
